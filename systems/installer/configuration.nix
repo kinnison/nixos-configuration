17d3466b50023b30757e35e25047a99884fb4fe2
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2024-10-18 11:23:37 +0100

    installer: Shuffle the installer a little more so that things work

--- systems/installer/configuration.nix
+++ systems/installer/configuration.nix
@@ -1,7 +1,7 @@
 # This is the nixos installer for my stuff
 
 { modulesPath, homes, lib, config, ... }: {
-  imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" ];
+  imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-base.nix" ];
   system.stateVersion = "24.05";
   networking.hostName = "installer";
   kinnison.user = {
@@ -13,6 +13,10 @@
     enable = true;
     wayland.enable = true;
   };
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = config.kinnison.user.name;
+  };
   kinnison.sound.enable = true;
   kinnison.network-manager.enable = true;
 
@@ -20,4 +24,6 @@
   hardware.cpu.intel.updateMicrocode =
     lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  boot.initrd.systemd.enable = lib.mkForce false;
+  boot.loader.systemd-boot.enable = false;
 }
