9421831f8a26a4335355bbe0ca486e1490e7f0a8
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-10-27 22:08:06 +0000

    bitwarden: Add rofi-rbw too
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- home-manager-modules/bitwarden.nix
+++ home-manager-modules/bitwarden.nix
@@ -1,8 +1,9 @@
 # The various Bitwarden related things
-{ config, lib, pkgs, ... }:
+{ osConfig, config, lib, pkgs, ... }:
 with lib;
 let
   cfg = config.kinnison.bitwarden;
+  guicfg = osConfig.kinnison.gui;
   pinentry-rofi = pkgs.kinnison.pinentry-rofi.override {
     rofi = config.programs.rofi.package;
   };
@@ -21,16 +22,24 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    programs.rbw = {
-      enable = true;
-      settings = {
-        email = cfg.email;
-        lock_timeout = 300;
-        pinentry = pinentry-rofi;
-        base_url = cfg.vault;
+  config = mkMerge [
+    (mkIf cfg.enable {
+      programs.rbw = {
+        enable = true;
+        settings = {
+          email = cfg.email;
+          lock_timeout = 300;
+          pinentry = pinentry-rofi;
+          base_url = cfg.vault;
+        };
       };
-    };
-  };
+    })
+    (mkIf (cfg.enable && guicfg.enable && guicfg.wayland.enable) {
+      home.packages = [ pkgs.rofi-rbw-wayland ];
+    })
+    (mkIf (cfg.enable && guicfg.enable && !guicfg.wayland.enable) {
+      home.packages = [ pkgs.rofi-rbw ];
+    })
+  ];
 }
 
