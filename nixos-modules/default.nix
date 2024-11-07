4ff2c67e908c9b28f9dc0936b701138dc3637155
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2024-11-07 09:01:11 +0000

    zram: Add zram as on by default
    Signed-off-by: Daniel Silverstone <daniel.silverstone@codethink.co.uk>

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -1,5 +1,27 @@
 # The various packages provided directly by my system and home configs
-{ ... }: {
+{ lib, config, ... }:
+with lib;
+let zram = config.kinnison.zram;
+in {
   imports =
     [ ./user.nix ./nm.nix ./simple ./gui ./sound ./bluetooth.nix ./power.nix ];
+
+  options.kinnison.zram = {
+    enable = mkEnableOption "Turn on zram";
+    percent = mkOption {
+      type = types.int;
+      default = 50;
+      description = "Percentage of RAM to permit to be zram swap";
+    };
+  };
+
+  config = mkMerge [
+    { kinnison.zram = mkDefault true; }
+    (mkIf zram.enable {
+      zramSwap = {
+        enable = true;
+        memoryPercent = zram.percent;
+      };
+    })
+  ];
 }
