483ee9ae69f4ea2e7f6930c3c6b5feed44a38bba
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-11-10 20:37:58 +0000

    pinentry: Fix up path better
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- packages/pinentry-rofi/default.nix
+++ packages/pinentry-rofi/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchFromGitHub, bash, rofi }:
+{ lib, stdenv, fetchFromGitHub, makeWrapper, bash, rofi, coreutils, gnused, gawk
+}:
 stdenv.mkDerivation {
   pname = "pinentry-rofi";
   version = "main";
@@ -9,6 +10,8 @@ stdenv.mkDerivation {
     hash = "sha256-R4SFHrTHmE8wFza6tvEBMbcVE7/0S9mTFPNi8QOp0Zw=";
   };
 
+  nativeBuildInputs = [ makeWrapper ];
+
   buildPhase = ''
     # Nothing to do
   '';
@@ -17,6 +20,9 @@ stdenv.mkDerivation {
     mkdir -p $out/bin
     sed -E -e's@^#!.*$@#!${bash}/bin/bash@' -e's@^ROFI="[^ ]+@ROFI="${rofi}/bin/rofi@' < src/pinentry-rofi.sh > $out/bin/pinentry-rofi
     chmod +x $out/bin/pinentry-rofi
+    wrapProgram $out/bin/pinentry-rofi --prefix PATH : ${
+      lib.makeBinPath [ coreutils gnused gawk ]
+    }
   '';
 
   meta.mainProgram = "pinentry-rofi";
