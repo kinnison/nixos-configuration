b7e887761b49f039ab1bee3e74674450fb3a65a0
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-11-23 10:16:09 +0000

    rust: Refactor use of config.xdg
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- home-manager-modules/rust.nix
+++ home-manager-modules/rust.nix
@@ -1,7 +1,9 @@
 # Rust configuration for home directory
 { config, lib, pkgs, ... }:
 with lib;
-let cfg = config.kinnison.rust;
+let
+  cfg = config.kinnison.rust;
+  dataHome = config.xdg.dataHome;
 in {
   options.kinnison.rust = {
     enable = mkEnableOption "Rust (via rustup)";
@@ -15,9 +17,9 @@ in {
   config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
     home.sessionVariables = {
-      RUSTUP_HOME = "${xdg.dataHome}/rustup";
-      CARGO_HOME = "${xdg.dataHome}/cargo";
+      RUSTUP_HOME = "${dataHome}/rustup";
+      CARGO_HOME = "${dataHome}/cargo";
     };
-    home.sessionPath = [ "${xdg.dataHome}/cargo/bin" ];
+    home.sessionPath = [ "${dataHome}/cargo/bin" ];
   };
 }
