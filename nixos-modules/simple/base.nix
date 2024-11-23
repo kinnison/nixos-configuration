be757d28a7262aa859709776757eb3eee2531253
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-11-23 16:56:52 +0000

    base: Remove lib. prefixes
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- nixos-modules/simple/base.nix
+++ nixos-modules/simple/base.nix
@@ -1,19 +1,20 @@
 # Base Role for all systems which I want
-{ config, lib, pkgs, ... }: {
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
+{ config, lib, pkgs, ... }:
+with lib; {
+  boot.loader.systemd-boot.enable = mkDefault true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.tmp.cleanOnBoot = true;
 
-  time.timeZone = lib.mkDefault "Europe/London";
-  i18n.defaultLocale = lib.mkDefault "en_GB.UTF-8";
-  environment.variables.LANG = lib.mkDefault "en_GB.UTF-8";
-  console.keyMap = lib.mkDefault "uk";
+  time.timeZone = mkDefault "Europe/London";
+  i18n.defaultLocale = mkDefault "en_GB.UTF-8";
+  environment.variables.LANG = mkDefault "en_GB.UTF-8";
+  console.keyMap = mkDefault "uk";
 
   users = { defaultUserShell = pkgs.zsh; };
 
   security.sudo = {
-    enable = lib.mkDefault true;
+    enable = mkDefault true;
     extraConfig = ''
       Defaults lecture = never
     '';
@@ -28,9 +29,9 @@
     '';
   };
 
-  environment.pathsToLink = lib.mkMerge [
+  environment.pathsToLink = mkMerge [
     [ "/share/zsh" ]
-    (lib.mkIf config.kinnison.gui.enable [
+    (mkIf config.kinnison.gui.enable [
       "/share/xdg-desktop-portal"
       "/share/applications"
     ])
@@ -38,7 +39,7 @@
 
   # We pretty much always want SSH and can mkForce it off later if needs be
   services.sshd.enable = true;
-  kinnison.impermanence.files = lib.mkIf config.services.sshd.enable [
+  kinnison.impermanence.files = mkIf config.services.sshd.enable [
     "/etc/ssh/ssh_host_ed25519_key"
     "/etc/ssh/ssh_host_ed25519_key.pub"
     "/etc/ssh/ssh_host_rsa_key"
@@ -48,7 +49,7 @@
   # We are not prudish about non-free software for the most part,
   # though we do limit it, so here we list what's allowed
   nixpkgs.config.allowUnfreePredicate = pkg:
-    builtins.elem (lib.getName pkg) [
+    builtins.elem (getName pkg) [
       "vscode"
       "vscode-extension-ms-vscode-cpptools"
       "vscode-extension-ms-vscode-remote-remote-ssh"
