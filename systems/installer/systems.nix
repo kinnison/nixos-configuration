f2eb7d0114f01b24435ea729ed04e5a1b8a11cf0
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-12-06 13:55:18 +0000

    installer: Switch to install closure rather than flake hack
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- systems/installer/systems.nix
+++ systems/installer/systems.nix
@@ -3,44 +3,31 @@
 let
   allSystemNames = builtins.attrNames systems;
   systemNames = builtins.filter (n: n != "installer") allSystemNames;
-  topLevel = system: systems.${system}.config.system.build.toplevel;
-  bootLoader = system: systems.${system}.config.system.build.installBootLoader;
+  deps-for-closure-for = system: [
+    systems.${system}.config.system.build.toplevel
+    systems.${system}.config.system.build.diskoScript
+    systems.${system}.config.system.build.diskoScript.drvPath
+    systems.${system}.pkgs.stdenv.drvPath
+    # Perl packages needed for systemd-boot setup
+    systems.${system}.pkgs.perlPackages.ConfigIniFiles
+    systems.${system}.pkgs.perlPackages.FileSlurp
+    # And the packages closure itself
+    (systems.${system}.pkgs.closureInfo { rootPaths = [ ]; }).drvPath
+  ];
+  closure-for-systems = let
+    alldeps = builtins.concatMap deps-for-closure-for systemNames;
+    mydeps =
+      builtins.map (i: i.outPath) (builtins.attrValues flakeInputs.self.inputs);
+  in pkgs.closureInfo { rootPaths = alldeps ++ mydeps; };
   flake = flakeInputs.self;
   installer = system:
     pkgs.writeShellScriptBin "disko-install-${system}" ''
       disko-install -f ${flake}#${system} --system-config '{"kinnison":{"installer-image":true}}' "$@"
     '';
-  bootloader-installer = system: {
-    name = "bootloader-installers/${system}";
-    value = { source = bootLoader system; };
-  };
   installers = builtins.map (installer)
     (builtins.filter (name: !lib.hasSuffix "-installable" name) systemNames);
-  bootloader-installers = builtins.map (bootloader-installer) systemNames;
-  toplevels = builtins.map (system: {
-    name = "toplevels/${system}";
-    value = { source = topLevel system; };
-  }) systemNames;
-  flakeMap = base: fin:
-    let
-      flakeIsToplevel = fname: fin.${fname} ? toplevelMarker;
-      fnames = builtins.filter (n: n != "self" && !(flakeIsToplevel n))
-        (builtins.attrNames fin);
-      flakes = builtins.map (fname: {
-        name = "${base}${fname}";
-        value = { source = fin.${fname}; };
-      }) fnames;
-      submaps = builtins.map (fname:
-        let
-          subbase = "${base}${fname}_";
-          f = fin.${fname};
-          finputs = if f ? inputs then f.inputs else { };
-        in flakeMap subbase finputs) fnames;
-      allsubs = builtins.concatLists submaps;
-    in flakes ++ allsubs;
-  flakes = flakeMap "flakes/" flakeInputs;
 in {
   environment.systemPackages = installers;
-  environment.etc =
-    builtins.listToAttrs (toplevels ++ flakes ++ bootloader-installers);
+  environment.etc."install-closure".source =
+    "${closure-for-systems}/store-paths";
 }
