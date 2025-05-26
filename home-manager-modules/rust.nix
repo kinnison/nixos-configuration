5951c8525345713200692dde27b166e2c83ce11c
Daniel Silverstone <dsilvers@digital-scurf.org>
2025-05-26 09:58:52 +0100

    rust: Add helix capability into the rust module
    This provides configuration for rust-analyzer if helix is enabled,
    ensuring that our experience is similar to that of vscode.
    
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- home-manager-modules/rust.nix
+++ home-manager-modules/rust.nix
@@ -4,6 +4,7 @@ with lib;
 let
   cfg = config.kinnison.rust;
   dataHome = config.xdg.dataHome;
+  hasHelix = config.kinnison.helix.enable;
 in {
   options.kinnison.rust = {
     enable = mkEnableOption "Rust (via rustup)";
@@ -14,12 +15,52 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    home.packages = [ cfg.package ];
-    home.sessionVariables = {
-      RUSTUP_HOME = "${dataHome}/rustup";
-      CARGO_HOME = "${dataHome}/cargo";
-    };
-    home.sessionPath = [ "${dataHome}/cargo/bin" ];
-  };
+  config = mkMerge [
+    (mkIf cfg.enable {
+      home.packages = [ cfg.package ];
+      home.sessionVariables = {
+        RUSTUP_HOME = "${dataHome}/rustup";
+        CARGO_HOME = "${dataHome}/cargo";
+      };
+      home.sessionPath = [ "${dataHome}/cargo/bin" ];
+    })
+    (mkIf (cfg.enable && hasHelix) {
+      programs.helix.languages = {
+        language-server.rust-analyzer.config = {
+          assist.emitMustUse = true;
+          cargo = {
+            buildScripts.enable = true;
+            allTargets = true;
+          };
+          check = {
+            allTargets = true;
+            command = "clippy";
+            workspace = true;
+          };
+          diagnostics.styleLints.enable = true;
+          inlayHints = {
+            closureCaptureHints.enable = true;
+            closureReturnTypeHints.enable = true;
+            implicitDrops.enable = true;
+            lifetimeElisionHints.enable = true;
+            lifetimeElisionHints.useParameterNames = true;
+            maxLength = 40;
+          };
+          lens = {
+            enable = true;
+            references = {
+              adt.enable = true;
+              enumVariant.enable = true;
+              trait.enable = true;
+              run.enable = false;
+            };
+          };
+          procMacro = {
+            enable = true;
+            attributes.enable = true;
+          };
+        };
+      };
+    })
+  ];
 }
