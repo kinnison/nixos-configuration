85db79d289974bab3e2c5b024d6a58be3d13cd53
Daniel Silverstone <daniel.silverstone@codethink.co.uk>
2025-06-04 09:02:44 +0100

    packages: Only include vintagestory on system builds
    Signed-off-by: Daniel Silverstone <daniel.silverstone@codethink.co.uk>

--- packages/default.nix
+++ packages/default.nix
@@ -1,9 +1,20 @@
 # The various packages provided directly by my system and home configs
-{ pkgs }: {
+{ pkgs }:
+let
+
+  is_system_build = pkgs ? kinnison;
+
+  only_system_pkgs = {
+
+    vintagestory = pkgs.callPackage ./upstream/vintagestory.nix { };
+  };
+
+  append_pkgs = if is_system_build then only_system_pkgs else { };
+
+in {
   rofi-lock = pkgs.callPackage ./rofi-lock { };
   pinentry-rofi = pkgs.callPackage ./pinentry-rofi { };
   capture = pkgs.callPackage ./capture { };
   stlink-udev = pkgs.callPackage ./stlink-udev { };
   qxw = pkgs.callPackage ./qxw { };
-  vintagestory = pkgs.callPackage ./upstream/vintagestory.nix { };
-}
+} // append_pkgs
