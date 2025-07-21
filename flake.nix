bd4d9adf6eac7dc8cb51b0818a7c9ee1e8de961c
Daniel Silverstone <dsilvers@digital-scurf.org>
2025-07-21 20:16:38 +0100

    flake: Fix extra-substituters
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- flake.nix
+++ flake.nix
@@ -262,7 +262,7 @@
     };
 
   nixConfig = {
-    extraSubstituters = [ "https://attic.infrafish.uk/kinnison" ];
+    extra-substituters = [ "https://attic.infrafish.uk/kinnison" ];
     extra-trusted-public-keys =
       [ "kinnison:OzaUTPRf8RkUjh52VIW61ZxePd4JhVSvzoL7/SpnxaQ=" ];
   };
