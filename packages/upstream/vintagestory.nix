adf14e91dcd644ba64c194e2ab92c41dff015f1d
Daniel Silverstone <dsilvers@digital-scurf.org>
2025-10-18 12:38:13 +0100

    vintagestory: Update to 1.21.5
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- packages/upstream/vintagestory.nix
+++ packages/upstream/vintagestory.nix
@@ -4,12 +4,12 @@
 
 stdenv.mkDerivation rec {
   pname = "vintagestory";
-  version = "1.21.1";
+  version = "1.21.5";
 
   src = fetchurl {
     url =
       "https://cdn.vintagestory.at/gamefiles/stable/vs_client_linux-x64_${version}.tar.gz";
-    hash = "sha256-6b0IXzTawRWdm2blOpMAIDqzzv/S7O3c+5k7xOhRFvI=";
+    hash = "sha256-dG1D2Buqht+bRyxx2ie34Z+U1bdKgi5R3w29BG/a5jg=";
   };
 
   nativeBuildInputs = [ makeWrapper copyDesktopItems ];
