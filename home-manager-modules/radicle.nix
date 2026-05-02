5eedb230305212991a7697af6de3d078be1b7e80
Daniel Silverstone <dsilvers@digital-scurf.org>
2026-05-02 06:56:56 +0100

    radicle: Add radicle-desktop wrapper
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- home-manager-modules/radicle.nix
+++ home-manager-modules/radicle.nix
@@ -1,9 +1,16 @@
 # The various Radicle related things
-{ osConfig, config, lib, pkgs, ... }:
+{ osConfig, config, lib, pkgs, writeShellApplication, ... }:
 with lib;
 let
   cfg = config.kinnison.radicle;
   guicfg = osConfig.kinnison.gui;
+  radicle-desktop = pkgs.writeShellApplication {
+    name = "radicle-desktop";
+    runtimeInputs = [ pkgs.radicle-desktop ];
+    text = ''
+      env __NV_DISABLE_EXPLICIT_SYNC=1 ${pkgs.radicle-desktop}/bin/radicle-desktop "$@"
+    '';
+  };
 in {
   options.kinnison.radicle = {
     enable = mkEnableOption "Radicle support";
@@ -20,7 +27,7 @@ in {
       home.packages = with pkgs; [ radicle-node ];
     })
     (mkIf (cfg.enable && guicfg.enable) {
-      home.packages = with pkgs; [ radicle-desktop ];
+      home.packages = [ radicle-desktop ];
 
       kinnison.allowedTCPPorts = mkIf cfg.allowListen [ 8776 ];
     })
